f19ae3cbdc4895b5842c6df830a6cf3488e94888
lizardkingLK <chansanfdo@gmail.com>
2024-10-30 20:19:09 +0530

    Resolved import issue

--- src/components/ScrolledCount/index.tsx
+++ src/components/ScrolledCount/index.tsx
@@ -24,7 +24,7 @@ import { ContainerProps, ScrolledCountProps } from "./ScrolledCount.types";
  * @returns component to render
  *
  */
-export default function ScrolledCount({
+function ScrolledCount({
   count = 0,
   fontSize = 80,
   separator = ",",
@@ -150,3 +150,4 @@ export default function ScrolledCount({
   );
 }
 
+export default ScrolledCount;
